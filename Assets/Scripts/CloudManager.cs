bc615446635324f8d93af2b4bbd61b7460486e24
HexagonalCube <83779758+HexagonalCube@users.noreply.github.com>
2024-02-25 05:59:40 -0300

    Update CloudManager.cs

--- Assets/Scripts/CloudManager.cs
+++ Assets/Scripts/CloudManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class CloudManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Color cloudColor;
+    public float colorIntensity;
+    public float cloudScale;
+    public Vector2 cloudSpeed;
+    public float cloudHeight;
+    public float cloudPower;
+
+    public Transform clouds;
+
+    [SerializeField] Material cloudMat;
+
     void Start()
     {
-        
+        SetVariables();
     }
-
-    // Update is called once per frame
-    void Update()
+    void SetVariables() //Sempre ter as mesmas variaveis
     {
-        
+        cloudMat = clouds.GetComponent<Renderer>().sharedMaterial;
+    }
+    private void OnValidate() //Sempre tenha os valores certos, e atualize eles no editor
+    {
+        if (!cloudMat)
+        {
+            SetVariables();
+        }
+        UpdateMaterial();
+    }
+    public void UpdateMaterial() //Atualizando material no editor
+    {
+        cloudMat.SetColor("_CloudColor", cloudColor * colorIntensity);
+        cloudMat.SetFloat("_CloudScale", cloudScale);
+        cloudMat.SetVector("_CloudSpeed", cloudSpeed / 1000);
+        cloudMat.SetVector("_DistortSpeed", -cloudSpeed / 1000);
+        cloudMat.SetFloat("_VertexOffset", cloudHeight);
+        cloudMat.SetFloat("_CloudPower", cloudPower / 100);
     }
 }
