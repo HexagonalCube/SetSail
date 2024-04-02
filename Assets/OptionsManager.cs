b56f1d092ef7a3e44caa3190b1a45da34d98c8c3
HexagonalCube <83779758+HexagonalCube@users.noreply.github.com>
2024-04-02 04:41:58 -0300

    Config Page in Pause Menu

---
+++ Assets/OptionsManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+/// <summary>
+/// Player Options Script
+/// </summary>
+public class OptionsManager : MonoBehaviour
+{
+    [SerializeField] Resolution[] fullScreenResolutionsAvailable;
+    [SerializeField] List<string> fullScreenResolutionList;
+    [SerializeField] TMP_Dropdown resolutionSelector;
+    [SerializeField] bool fullScreen = true;
+    [SerializeField] Resolution selectedResolution;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] AudioMixer masterVolume;
+    private void Start() //Gets Fisrt-Time and previously set options
+    {
+        fullScreenResolutionsAvailable = Screen.resolutions;
+        GetResolutions();
+        fullScreen = SaveGame.LoadFullscreen();
+        selectedResolution = fullScreenResolutionsAvailable[SaveGame.LoadResolutionIndex()];
+        Debug.Log($"Previously set resolution {SaveGame.LoadResolutionIndex()}");
+        UpdateResolution();
+        LoadVolume();
+    }
+    void GetResolutions() //Gets all available resolutions, & sets player default to highest
+    {
+        for (int i = 0; i < fullScreenResolutionsAvailable.Length; i++)
+        {
+            //float w = fullScreenResolutionsAvailable[i].width;
+            //float h = fullScreenResolutionsAvailable[i].height;
+            fullScreenResolutionList.Add($"{fullScreenResolutionsAvailable[i].width} x {fullScreenResolutionsAvailable[i].height} ({fullScreenResolutionsAvailable[i].refreshRateRatio}Hz)");
+        }
+        resolutionSelector.AddOptions(fullScreenResolutionList);
+        resolutionSelector.SetValueWithoutNotify(SaveGame.LoadResolutionIndex());
+        if (PlayerPrefs.GetInt("FRESH",0) == 0)
+        {
+            resolutionSelector.SetValueWithoutNotify(fullScreenResolutionList.Count-1);
+            SetResolution(fullScreenResolutionList.Count-1);
+        }
+    }
+    public void MakeFullscreen(bool fs) //Toggles & saves fullscreen
+    {
+        fullScreen = fs;
+        SaveGame.SaveFullscreen(fs);
+        UpdateResolution();
+    }
+    public void SetResolution(int res) //Sets resolution in list Index
+    {
+        selectedResolution = fullScreenResolutionsAvailable[res];
+        SaveGame.SaveResolutionIndex(res);
+        PlayerPrefs.SetInt("FRESH",1);
+        Debug.Log($"Resolution Index Set {res}");
+        UpdateResolution();
+    }
+    void UpdateResolution() //Updates screen resolution
+    {
+        //Screen.SetResolution(selectedResolution.width, selectedResolution.height, fullScreen, selectedResolution.refreshRate);
+        if (fullScreen)
+        {
+            Screen.SetResolution(selectedResolution.width, selectedResolution.height, FullScreenMode.ExclusiveFullScreen, selectedResolution.refreshRateRatio);
+        }
+        else
+        {
+            Screen.SetResolution(selectedResolution.width, selectedResolution.height, FullScreenMode.Windowed, selectedResolution.refreshRateRatio);
+        }
+    }
+    public void SetVolume(float vol) //saves selected volume & sets volume to log10 scale
+    {
+        SaveGame.SaveVolume(vol);
+        float convertedVolume = Mathf.Log10(vol) * 20;
+        masterVolume.SetFloat("MasterVolume", convertedVolume);
+    }
+    void LoadVolume() //Loads saved volume & converts to log10 scale
+    {
+        volumeSlider.SetValueWithoutNotify(SaveGame.LoadVolume());
+        float convertedVolume = Mathf.Log10(SaveGame.LoadVolume()) * 20;
+        masterVolume.SetFloat("MasterVolume",convertedVolume);
+        Debug.Log($"VolumeLoaded {SaveGame.LoadVolume()}");
+    }
+}
--- Assets/PauseMenuScript.cs
+++ Assets/PauseMenuScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PauseMenuScript : MonoBehaviour
 {
+    enum Panel { Config, Story, Tips }
     [SerializeField] GameUI_Controller ui;
+    [SerializeField] GameObject configPanel;
+    [SerializeField] GameObject storyPanel;
 
     public void OnCloseButtonClick()
     {
@@ -14,8 +17,33 @@ public class PauseMenuScript : MonoBehaviour
     {
         Application.Quit();
     }
-    void OnConfigButtonClick()
+    public void OnConfigButtonClick()
     {
+        PanelSwitcher(Panel.Config);
+    }
+    public void OnStoryButtonClick()
+    {
+        PanelSwitcher(Panel.Story);
+    }
+    void DisableAllPanels() //Disables all panels prior to activation
+    {
+        configPanel.SetActive(false);
+        storyPanel.SetActive(false);
+    }
+    void PanelSwitcher(Panel page)
+    {
+        DisableAllPanels();
+        switch (page)
+        {
+            case Panel.Config:
+                configPanel.SetActive(true);
+                break;
+            case Panel.Story:
+                storyPanel.SetActive(true);
+                break;
+            case Panel.Tips:
 
+                break;
+        }
     }
 }
