95bfc53cac6a0b7b4208126768ed79f777712261
HexagonalCube <83779758+HexagonalCube@users.noreply.github.com>
2024-05-09 21:56:06 -0300

    Bud Loves yapping

--- Assets/Scripts/OceanFollower.cs
+++ Assets/Scripts/OceanFollower.cs
@@ -13,22 +13,33 @@ public class OceanFollower : MonoBehaviour
 
     void Update() //Makes the object follow the player within the assigned grid
     {
-        if (CameraScript.Instance.InBoat)
+        if (CameraScript.Instance != null)
+        {
+            if (CameraScript.Instance.InBoat)
+            {
+                Vector3 boatPos = boat.position;
+                boatPos = new Vector3(Mathf.FloorToInt(boatPos.x / gridSnap), 0, Mathf.FloorToInt(boatPos.z / gridSnap));
+                boatPos *= gridSnap;
+                //Debug.Log($"Calculated {boatPos} Real{boat.position}");
+
+                transform.position = new Vector3(boatPos.x, transform.position.y, boatPos.z);
+            }
+            else
+            {
+                Vector3 pPos = player.position;
+                pPos = new Vector3(Mathf.FloorToInt(pPos.x / gridSnap), 0, Mathf.FloorToInt(pPos.z / gridSnap));
+                pPos *= gridSnap;
+
+                transform.position = new Vector3(pPos.x, transform.position.y, pPos.z);
+            }
+        }
+        else
         {
             Vector3 boatPos = boat.position;
             boatPos = new Vector3(Mathf.FloorToInt(boatPos.x / gridSnap), 0, Mathf.FloorToInt(boatPos.z / gridSnap));
             boatPos *= gridSnap;
-            //Debug.Log($"Calculated {boatPos} Real{boat.position}");
 
             transform.position = new Vector3(boatPos.x, transform.position.y, boatPos.z);
-        }
-        else
-        {
-            Vector3 pPos = player.position;
-            pPos = new Vector3(Mathf.FloorToInt(pPos.x / gridSnap), 0, Mathf.FloorToInt(pPos.z / gridSnap));
-            pPos *= gridSnap;
-
-            transform.position = new Vector3(pPos.x, transform.position.y, pPos.z);
         }
     }
 }
