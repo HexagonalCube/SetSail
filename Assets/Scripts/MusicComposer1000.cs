e082fed77ea1b2c37900dfc38411c8355b2d17d8
HexagonalCube <83779758+HexagonalCube@users.noreply.github.com>
2024-05-13 23:50:33 -0300

    Islands Music In-Game & Player Camera Fixed

--- Assets/Scripts/MusicComposer1000.cs
+++ Assets/Scripts/MusicComposer1000.cs
@@ -8,6 +8,11 @@ public class MusicComposer1000 : MonoBehaviour
     [SerializeField] AudioClip[] musicPlucks;
     [SerializeField] AudioClip[] storyMusic;
     [SerializeField] AudioSource source;
+    int curClip;
+    int previousClip;
+    int previousClip2;
+    bool plucks = false;
+    bool fading = false;
     private void Awake()
     {
         if (Instance != null)
@@ -19,37 +24,91 @@ public class MusicComposer1000 : MonoBehaviour
     private void Start()
     {
         source = GetComponent<AudioSource>();
+        //StartCoroutine(GameStart());
+        StartStoryMusic(0);
     }
-    private void FixedUpdate()
+    private void Update()
     {
-        
-    }
-    public void StartStoryMusic()
-    {
-        SoundFade(0.01f);
-        WaitForNextTrack(storyMusic[0], 0.3f);
-    }
-    void GameStart()
-    {
-        source.PlayOneShot(source.clip);
-    }
-    void SoundFade(float strenght)
-    {
-        for (float i = source.volume; i > 0;)
+        if (plucks && !source.isPlaying)
         {
-            source.volume -= strenght;
-            if (source.volume <= 0)
-            {
-                source.volume = 0;
-                source.Stop();
-            }
+            RandomizeTimeTracks(musicPlucks, 1f);
+        }
+    }
+    public void StartStoryMusic(int index)
+    {
+        StartCoroutine(SoundFade(0.01f));
+        StartCoroutine(WaitForNextTrack(storyMusic[index], 1f));
+        source.loop = true;
+        plucks = false;
+    }
+    public void StartPlucksMusic()
+    {
+        StartCoroutine(SoundFade(0.001f));
+        source.loop = false;
+        plucks = true;
+    }
+    public void FadeCurTrack(float strenght)
+    {
+        StartCoroutine(SoundFade(strenght));
+    }
+    IEnumerator GameStart()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        //Debug.Log("A");
+        StartStoryMusic(0);
+    }
+    IEnumerator SoundFade(float strenght, float i = 1)
+    {
+        //Debug.Log("B");
+        fading = true;
+        yield return new WaitForFixedUpdate();
+        source.volume = Mathf.Lerp(0, source.volume, i);
+        i -= strenght;
+        Debug.Log($"{i} {strenght} {source.volume}");
+        if (source.volume <= 0)
+        {
+            source.volume = 0;
+            source.Stop();
+            fading = false;
+            //Debug.Log("STOP");
+        }
+        else
+        {
+            //Debug.Log("C");
+            StartCoroutine(SoundFade(strenght, i));
         }
     }
     IEnumerator WaitForNextTrack(AudioClip clip, float volume)
     {
+        //Debug.Log("Waiting");
         yield return new WaitWhile(() => source.isPlaying);
+        yield return new WaitWhile(() => fading);
+        //Debug.Log("GO");
         source.clip = clip;
         source.volume = volume;
         source.Play();
+    }
+    IEnumerator RandomizeTimeTracks(AudioClip[] clips, float volume)
+    {
+        yield return new WaitWhile(() => source.isPlaying);
+        yield return new WaitForSecondsRealtime(Random.Range(5f, 15f));
+        AudioClip clip = PickRandomClip(clips);
+        StartCoroutine(WaitForNextTrack(clip, volume));
+    }
+    AudioClip PickRandomClip(AudioClip[] clips, int tries = 0)
+    {
+        tries++;
+        while (previousClip == curClip || curClip == previousClip2)
+        {
+            curClip = Random.Range(0,clips.Length);
+        }
+        if (curClip != previousClip && curClip != previousClip2 || tries > 6)
+        {
+            return clips[curClip];
+        }
+        else
+        {
+            return PickRandomClip(clips, tries);
+        }
     }
 }
