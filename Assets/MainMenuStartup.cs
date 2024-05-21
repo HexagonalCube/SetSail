809dfe89c7c878ec36a51cbb43972fb1cb20ae6e
HexagonalCube <83779758+HexagonalCube@users.noreply.github.com>
2024-05-21 17:51:02 -0300

    Boop

---
+++ Assets/MainMenuStartup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MainMenuStartup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindAnyObjectByType(typeof(WeatherController)).GetComponent<WeatherController>().UpdateWeather();
+        Time.timeScale = 1.0f;
+    }
+}
